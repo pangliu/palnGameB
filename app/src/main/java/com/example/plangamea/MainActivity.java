0f84501b3c44a805d1b814748d320c0db6e9a28a
hankliu <hank@glinteagle.com>
2023-02-04 14:43:07 +0800

    [modify]set text

--- app/src/main/java/com/example/plangamea/MainActivity.java
+++ app/src/main/java/com/example/plangamea/MainActivity.java
@@ -2,13 +2,19 @@ package com.example.plangamea;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView tvHellow;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvHellow = (TextView) findViewById(R.id.tv_hellow);
+        tvHellow.setText("hahahah");
+
     }
 }
